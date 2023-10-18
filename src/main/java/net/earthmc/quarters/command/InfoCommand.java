427a7b1ea3703d60e2360b90a8a4979dd5380e84
Fruitloopins <49851457+Fruitloopins@users.noreply.github.com>
2023-10-19 02:41:20 +1100

    Improve info command

--- src/main/java/net/earthmc/quarters/command/InfoCommand.java
+++ src/main/java/net/earthmc/quarters/command/InfoCommand.java
@@ -5,10 +5,10 @@ import co.aikar.commands.annotation.*;
 import io.papermc.paper.plugin.configuration.PluginMeta;
 import net.earthmc.quarters.Quarters;
 import net.earthmc.quarters.api.QuartersMessaging;
+import net.earthmc.quarters.util.QuarterUtil;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.format.NamedTextColor;
-import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.command.CommandSender;
 
 @CommandAlias("quarters|q")
@@ -21,11 +21,13 @@ public class InfoCommand extends BaseCommand {
         PluginMeta meta = Quarters.INSTANCE.getPluginMeta();
 
         TextComponent component = Component.text()
-                        .append(Component.text("Version: ").color(NamedTextColor.DARK_GRAY).decorate(TextDecoration.ITALIC))
-                        .append(Component.text(meta.getVersion() + "\n")).color(NamedTextColor.GRAY)
-                        .append(Component.text("Authors: ").color(NamedTextColor.DARK_GRAY).decorate(TextDecoration.ITALIC))
-                        .append(Component.text(meta.getAuthors().get(0))).color(NamedTextColor.GRAY)
-                        .build();
+                .append(Component.text("Author: ").color(NamedTextColor.DARK_GRAY))
+                .append(Component.text("Fruitloopins ")).color(NamedTextColor.GRAY)
+                .append(Component.text("Version: ").color(NamedTextColor.DARK_GRAY))
+                .append(Component.text(meta.getVersion() + "\n")).color(NamedTextColor.GRAY)
+                .append(Component.text("Quarters: ").color(NamedTextColor.DARK_GRAY))
+                .append(Component.text(QuarterUtil.getAllQuarters().size())).color(NamedTextColor.GRAY)
+                .build();
 
         QuartersMessaging.sendInfoWall(sender, component);
     }
