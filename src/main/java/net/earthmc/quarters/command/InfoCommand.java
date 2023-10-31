4c5451a9dd421b995f09373bfba06e5cae0b9f93
Fruitloopins <49851457+Fruitloopins@users.noreply.github.com>
2023-10-31 14:12:34 +1100

    Add more info to /q info

--- src/main/java/net/earthmc/quarters/command/InfoCommand.java
+++ src/main/java/net/earthmc/quarters/command/InfoCommand.java
@@ -5,11 +5,16 @@ import co.aikar.commands.annotation.*;
 import io.papermc.paper.plugin.configuration.PluginMeta;
 import net.earthmc.quarters.Quarters;
 import net.earthmc.quarters.api.QuartersMessaging;
+import net.earthmc.quarters.object.Cuboid;
+import net.earthmc.quarters.object.Quarter;
 import net.earthmc.quarters.util.QuarterUtil;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.event.ClickEvent;
 import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.Style;
 import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.command.CommandSender;
 
 @CommandAlias("quarters|q")
@@ -21,13 +26,23 @@ public class InfoCommand extends BaseCommand {
     public void onInfo(CommandSender sender) {
         PluginMeta meta = Quarters.INSTANCE.getPluginMeta();
 
+        int numCuboids = 0;
+        for (Quarter quarter : QuarterUtil.getAllQuarters()) {
+            numCuboids += quarter.getCuboids().size();
+        }
+
         TextComponent component = Component.text()
-                .append(Component.text("Author: ").color(NamedTextColor.DARK_GRAY))
-                .append(Component.text("Fruitloopins ", TextColor.color(0xF6003c)))
-                .append(Component.text("Version: ").color(NamedTextColor.DARK_GRAY))
-                .append(Component.text(meta.getVersion() + "\n")).color(NamedTextColor.GRAY)
-                .append(Component.text("Quarters: ").color(NamedTextColor.DARK_GRAY))
-                .append(Component.text(QuarterUtil.getAllQuarters().size())).color(NamedTextColor.GRAY)
+                .append(Component.text("Author: ", NamedTextColor.DARK_GRAY))
+                .append(Component.text("Fruitloopins ", TextColor.color(0xF6003C)))
+                .append(Component.text("Version: ", NamedTextColor.DARK_GRAY))
+                .append(Component.text(meta.getVersion() + "\n", NamedTextColor.GRAY))
+                .append(Component.text("Quarters: ", NamedTextColor.DARK_GRAY))
+                .append(Component.text(QuarterUtil.getAllQuarters().size(), NamedTextColor.GRAY))
+                .append(Component.text(" Cuboids: ", NamedTextColor.DARK_GRAY))
+                .append(Component.text(numCuboids + "\n", NamedTextColor.GRAY))
+                .append(Component.text("Wiki", TextColor.color(0x2F81F7), TextDecoration.UNDERLINED).clickEvent(ClickEvent.openUrl("https://github.com/Fruitloopins/Quarters/wiki")))
+                .append(Component.text(" "))
+                .append(Component.text("Discord", TextColor.color(0x2F81F7), TextDecoration.UNDERLINED).clickEvent(ClickEvent.openUrl("https://discord.gg/ey6ZvnwAJp")))
                 .build();
 
         QuartersMessaging.sendInfoWall(sender, component);
